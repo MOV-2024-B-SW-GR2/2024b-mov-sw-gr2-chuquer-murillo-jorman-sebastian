b36e638a057a0469483a94a1e55a290f5e997ab2
Jorman Chuquer <jorman.chuquer@epn.edu.ec>
2024-11-07 08:43:08 -0500

    uso de clases y companion object

--- 00-Kotlin/02_Funciones/src/main/kotlin/Main.kt
+++ 00-Kotlin/02_Funciones/src/main/kotlin/Main.kt
@@ -6,6 +6,17 @@ fun main() {
 
     //ejemplo de named parameter
     val sueldoTotal:Double = calcularSueldo(10.00, bonoEspecial = 20.00);
+    val sumaA = Suma(1,1)
+    val sumaB = Suma(null,1)
+    val sumaC = Suma(1,null)
+    val sumaD = Suma(null,null)
+    sumaA.sumar()
+    sumaB.sumar()
+    sumaC.sumar()
+    sumaD.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
 }
 
 
@@ -55,5 +66,62 @@ class Suma(    // Constructor primario
     unoParametro,
     dosParametro
 ) {
+    //Clase 007
     // Cuerpo de la clase (puede contener métodos o propiedades adicionales)
+    public val publicoExplicito:String = "publicas"
+    val soypublicoImplicito:String = "publicas implicito"
+   //bloque del constructor primario
+    init {
+        this.numeroUno
+       this.numeroDos
+       numeroUno //this opcional para propiedades y métodos
+       numeroDos
+       this.soypublicoImplicito
+       soypublicoImplicito
+    }
+
+    constructor( //constructor secundario
+        //this llama al constructor primario
+    unoParametro: Int?, dosParametro: Int):this(
+        //necesitamos especificar que el nullable se convierta en 0 en caso de cumplirse
+        if(unoParametro == null) 0 else unoParametro,
+        dosParametro
+    ){
+        //bloque para el constructor secundario
+    }
+    //otros dos constructores para aceptar los otros tipos de nullable y casos
+    constructor(
+        unoParametro: Int, dosParametro: Int?
+
+    ):this(
+        if(dosParametro == null) 0 else dosParametro,
+    )
+    constructor(
+        unoParametro: Int?, dosParametro: Int?
+    ):this(
+        if(unoParametro == null) 0 else unoParametro,
+        if(dosParametro == null) 0 else dosParametro
+    )
+    fun sumar():Int{
+        val total = numeroUno + numeroDos
+        agregarHistorial(total)
+        return total
+    }
+    //registro de las veces que el usuario invoca a una función
+    //objeto que acompaña a todas las instancias de la clase (Singleton?) similar a static
+
+    companion object {
+        //compartir funciones y variables
+        val pi = 3.14
+        //Accesible: NombreClase.metodo,
+        //Suma.pi
+        fun elevarAlCuadrado(num:Int):Int{
+            return num*num
+        }
+        //Suma.elevarAlCuadrado()
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorTotalSuma:Int){
+            historialSumas.add(valorTotalSuma)
+        }
+    }
 }
